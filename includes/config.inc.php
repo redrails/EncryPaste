12c7438002abc062f2d4cd5bca98c7d522dd715c
redrails <redrailz@gmail.com>
2014-09-30 23:37:50 +0100

    Create config.inc.php

---
+++ includes/config.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+
+	Welcome to EncryPaste by Ihtasham;
+	
+	Please set up your dabase parameters here in order for everything to work.
+	
+	After this you can run /install.php for loading the tables
+	and other configurations.
+
+
+*/
+
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "encrypaste");
+
+?>
