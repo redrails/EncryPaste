d2695d7334d0d554139b24312a41dd049c0684ad
redrails <redrailz@gmail.com>
2014-10-02 23:46:38 +0100

    Create generate.use.php

---
+++ includes/generate.use.php
@@ -0,0 +1,17 @@
+<?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+class generate{
+
+	function RandomString($length) {
+
+		$this->keys = array_merge(range(0,9), range('a', 'z'));
+
+		for($i=0; $i < $length; $i++) {
+		    $this->key .= $this->keys[array_rand($this->keys)];
+		}
+		return $this->key;
+	}
+}
+
+?>
